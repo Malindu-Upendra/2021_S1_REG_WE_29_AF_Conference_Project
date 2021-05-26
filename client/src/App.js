6e18cc9200540029ec9c7f2a064a8c0ae47a62b1
MNM-Hussain <mnmhuss91@gmail.com>
2021-05-26 13:05:33 +0530

    all done to start

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,18 @@
-//structured
+import React, { Component } from 'react'
+import {render} from "react-dom";
+
+
+export class App extends Component{
+
+    render(){
+        return(
+
+            <>
+                <p> start</p>
+            </>
+        )
+
+    }
+}
+
+export  default App;
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; //importing the react
+import {render} from 'react-dom'; //render is the method is used to insert  a react application into html
+import App from "./App";
+
+
+
+
+
+render(<App/>, document.getElementById('app'));
