0d837ca74cf6eb0820eda2231cc588b0029ace81
MNM-Hussain <mnmhuss91@gmail.com>
2021-06-04 01:15:32 +0530

    updated the nav page of admin dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,36 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import Header from "./components/pages/Header/Header";
 import Footer from "./components/pages/Footer/Footer";
 import Section from "./components/Section";
+import AdminSection from "./components/AdminSection";
 
 export class App extends Component{
+    state={
+        user:'user',
+        Admin:'admin'
+    }
 
     render(){
         return(
 
          <Router>
+             { this.state.user==='user'  ?
+                 <>
              <Header/>
-
                 <Section/>
-
              <Footer/>
+
+        </>
+
+                 : null}
+
+
+             { this.state.Admin==='admin'  ?
+                 <>
+                     <AdminSection/>
+                 </>
+
+                 : null}
+
          </Router>
         )
 
