e125d1525c579a1e6962bf810f017baaf8b97f7b
MNM-Hussain <mnmhuss91@gmail.com>
2021-06-28 23:13:49 +0530

    Editor Frontend part , 3 insert (axios) done

--- client/src/components/pages/Editor/AddConferenceTracks/AddConferenceTracks.jsx
+++ client/src/components/pages/Editor/AddConferenceTracks/AddConferenceTracks.jsx
@@ -2,16 +2,52 @@ import {Component} from "react";
 import Card from 'react-bootstrap/Card'
 import {Button, Form} from "react-bootstrap";
 import {TextField} from "@material-ui/core";
+import axios from "axios";
 
 
 export class AddConferenceTracks extends Component{
+
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        heading: '',
+        description: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+                ...this.state.data,
+                [e.target.name]: e.target.value
+            }
+        );
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const  AddConferenceTrack ={
+            heading: this.state.heading,
+            description: this.state.description
+        };
+        console.log('Data send:', AddConferenceTrack)
+        axios.post('http://localhost:5000/editor/addConferenceTracksForm',AddConferenceTrack)
+            .then(response => {
+                alert('AddConferenceTracks data are successfully inserted')
+                window.location="/";
+            })
+            .catch(error => {
+                console.log(error.message);
+                alert(error.message)
+            })
+    }
 
     render() {
         return(
             <Card style={{ width: '50%',marginLeft: '30%',marginTop: '5%',height :'425px',marginBottom: '5%' }}>
                 <Card.Header className={"text-center"} as="h5">Add Conference Tracks</Card.Header>
                 <Card.Body>
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
 
                         <Form.Group className="mb-3" style={{height:'50px'}} controlId="ctitle">
                             <TextField style={{ width: '100%' }}
@@ -20,6 +56,10 @@ export class AddConferenceTracks extends Component{
                                        variant="outlined"
                                        multiline
                                        rows={1}
+                                       name="heading"
+                                       value={this.state.heading}
+                                       onChange={this.handleChange}
+
                             />
 
                         </Form.Group>
@@ -31,6 +71,9 @@ export class AddConferenceTracks extends Component{
                                        variant="outlined"
                                        multiline
                                        rows={8}
+                                       name="description"
+                                       value={this.state.description}
+                                       onChange={this.handleChange}
                             />
 
                         </Form.Group>
--- client/src/components/pages/Editor/CreateKeynotes/CreateKeynotes.jsx
+++ client/src/components/pages/Editor/CreateKeynotes/CreateKeynotes.jsx
@@ -1,10 +1,54 @@
 import {Component} from "react";
 import Card from "react-bootstrap/Card";
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
-import Icon from "@material-ui/core/Icon";
+import {Button, Col, Form, Row} from "react-bootstrap";
+import axios from "axios";
 
 
 export class CreateKeynotes extends Component{
+
+
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        title: '',
+        firstname: '',
+        lastname: '',
+        university: '',
+        description: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+
+                ...this.state.data,
+                [e.target.name]: e.target.value
+            }
+        );
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const  Keynote ={
+            title: this.state.title,
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            university: this.state.university,
+            description: this.state.description
+        };
+
+        console.log('Data send:', Keynote)
+        axios.post('http://localhost:5000/editor/keynotes',Keynote)
+            .then(response => {
+                alert('KeyNotes data successfully inserted')
+                 window.location="/";
+            })
+            .catch(error => {
+                console.log(error.message);
+                alert(error.message)
+            })
+    }
 
     render() {
         return(
@@ -15,59 +59,79 @@ export class CreateKeynotes extends Component{
                         <Card.Body>
                         <div className='container'>
 
-                            <Form>
+                            <Form onSubmit={this.handleSubmit}>
                                 <Row>
 
                                     <Col sm='2'>
-                                        <Form.Control placeholder="Title"  />
+                                        <Form.Control
+                                            placeholder="Title"
+                                            type="text"
+                                            name="title"
+                                            value={this.state.title}
+                                            onChange={this.handleChange}
+                                        />
                                     </Col>
 
                                     <Col sm='5'>
-                                        <Form.Control placeholder="First name"  />
+                                        <Form.Control
+                                            placeholder="First name"
+                                            type="text"
+                                            name="firstname"
+                                            value={this.state.firstname}
+                                            onChange={this.handleChange}
+                                        />
                                     </Col>
+
                                     <Col sm='5'>
-                                        <Form.Control placeholder="Last name" />
+                                        <Form.Control
+                                            placeholder="Last name"
+                                            type="text"
+                                            name="lastname"
+                                            value={this.state.lastname}
+                                            onChange={this.handleChange}
+                                        />
                                     </Col>
-
-
                                 </Row>
                                 <p></p>
+
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
                                     {/*<Form.Label>Email address</Form.Label>*/}
-                                    <Form.Control type="text" placeholder="University" />
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="University"
+                                        name="university"
+                                        value={this.state.university}
+                                        onChange={this.handleChange}
+                                    />
                                 </Form.Group>
-
 
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
                                     {/*<Form.Label>Password</Form.Label>*/}
-                                    <Form.Control as="textarea" rows="5" placeholder="Description" />
+                                    <Form.Control
+                                        as="textarea"
+                                        rows="5"
+                                        placeholder="Description"
+                                        name="description"
+                                        value={this.state.description}
+                                        onChange={this.handleChange}
+                                    />
                                 </Form.Group>
-
-
                                 <Form.Group className="mb-4" controlId="formBasicPassword">
-
                                     <Form.File
                                         id="custom-file-translate-scss"
                                         label="choose the Picture"
                                         custom
                                     />
-
-
                                 </Form.Group>
-
-
-                                <Button variant="primary" type="submit"
-                                >
+                                <Button variant="primary" type="submit">
                                     <i className="fas fa-plus-circle"></i> {}
                                      Add Keynotes
                                 </Button>
-
-
                             </Form>
                         </div>
                         </Card.Body>
                     </Card>
-                       </div>
+                    </div>
                 </div>
 
         )
